09176cd95cfac885fb49989114c36565891542bd
Håvard Berland <havb@equinor.com>
2024-10-21 20:05:01 +0200

    Extract function into a library file
    The library file can be compiled with gcc -c library.c

---
+++ main/library.c
@@ -0,0 +1,12 @@
+#ifdef ARDUINO
+    #include <Arduino.h>
+#else
+    #include <stdint.h>
+    #include <stdbool.h>
+#endif
+
+uint8_t getIpsFromDip(uint8_t value, bool isLocal) {
+  if (isLocal) return value + 100;
+  else if (value % 2 == 0) return value + 101;  // if even
+  else return value + 99;                       // if odd
+}
