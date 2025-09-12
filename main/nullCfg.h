7de7bfe98f2d0e081540a8a112b91c376ad2f33c
andrethuber <andrethuber@gmail.com>
2025-09-12 19:40:15 +0200

    Move config from dip methode to external files added with compiler options.

---
+++ main/nullCfg.h
@@ -0,0 +1,22 @@
+
+// Config file for null
+
+
+enum lineControllers {
+  null
+};
+
+const uint8_t localID = null;
+const uint8_t pairedID = null;
+
+const char lineName = "null";
+
+IPAddress ipAddresses[] = {
+  { 0, 0, 0, 0 }  // null
+};
+
+byte macAddress[] = { 0xaa, 0xaa, 0xaa, 0xaa, 0xaa, 0x00 };  // 'Ethernet.begin()' wants a MAC address as a argument, but it dosent seem to acually want to use it, as 'Ethernet.MACAddress()' returns something else.
+
+IPAddress localGateway = { 0, 0, 0, 0 };
+IPAddress remoteServerIp = { 0, 0, 0, 0 };
+
